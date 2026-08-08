17bd987ac74cb4f9fc640784abcbffde8de622a0
Wutipong Wongsakuldej <mr_tawan@hotmail.com>
2026-08-08 12:18:31 +0000

    Update devcontainer configuration and generated gRPC files; add protoc feature and adjust paths

--- backend/grpc/generate.go
+++ backend/grpc/generate.go
@@ -1,3 +1,3 @@
 package grpc
 
-//go:generate protoc  --go_out=. --go-grpc_out=. --proto_path=./schema --go_opt=paths=source_relative --go-grpc_opt=paths=source_relative manga.proto history.proto tag.proto types.proto maintenance.proto system.proto user.proto
+//go:generate protoc  --go_out=. --go-grpc_out=. --proto_path=../../grpc --go_opt=paths=source_relative --go-grpc_opt=paths=source_relative manga.proto history.proto tag.proto types.proto maintenance.proto system.proto user.proto
