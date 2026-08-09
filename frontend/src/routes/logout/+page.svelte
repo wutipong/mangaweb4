6aefeed21ad00c5b77479720c5a64aa9bded1f0e
Wutipong Wongsakuldej <mr_tawan@hotmail.com>
2026-08-09 00:50:17 +0000

    feat(auth): update logout handling and adjust OIDC logout URL

--- frontend/src/routes/logout/+page.svelte
+++ frontend/src/routes/logout/+page.svelte
@@ -2,6 +2,11 @@
 	import Container from '$lib/components/Container.svelte';
 	import Content from '$lib/components/Content.svelte';
 	import NavBar from '$lib/components/NavBar.svelte';
+	import { authClient } from '$lib/auth';
+
+	async function logOut() {
+		await authClient.signOut();
+	}
 </script>
 
 <svelte:head>
@@ -10,9 +15,9 @@
 
 <Container>
 	<Content>
-		<NavBar><div class="text-xl">Login</div></NavBar>
+		<NavBar><div class="text-xl">Logout</div></NavBar>
 		<div class="prose container mx-auto mt-4 max-w-5xl">
-			<h2>Logout from MangaWeb 4.</h2>
+			<button class="btn btn-wide btn-primary" onclick={async () => logOut()}>Logout</button>
 		</div>
 	</Content>
 </Container>
