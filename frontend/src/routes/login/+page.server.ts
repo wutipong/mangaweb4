4063a542a7ccde46ed76c9e787dba59ef3f5513a
Wutipong Wongsakuldej <mr_tawan@hotmail.com>
2026-08-09 11:54:40 +0000

    fix(auth): update callbackURL handling to use url.origin directly

--- frontend/src/routes/login/+page.server.ts
+++ frontend/src/routes/login/+page.server.ts
@@ -1,12 +1,10 @@
 import { env } from '$env/dynamic/private';
-import { browseURL } from '$lib/routes';
 import type { PageServerLoad } from './$types';
 
 export const load: PageServerLoad = async ({ url }) => {
 	return {
 		providerId: env.OIDC_PROVIDER_ID ?? '',
-		callbackURL: browseURL(url).toString(),
+		callbackURL: url.origin,
 		errorCallbackURL: '/error'
-		// disableRedirect: true,
 	};
 };
