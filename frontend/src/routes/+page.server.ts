8f7887d344947cd90015e8f43531c04efc2159fc
Wutipong Wongsakuldej <mr_tawan@hotmail.com>
2026-08-28 13:28:30 +0000

    fix: add session check in load function to redirect unauthenticated users

--- frontend/src/routes/+page.server.ts
+++ frontend/src/routes/+page.server.ts
@@ -3,18 +3,19 @@ import variables from '$lib/variables.server';
 import { redirect } from '@sveltejs/kit';
 import { browseURL, tagURL } from '$lib/routes';
 
-export const load: PageServerLoad = async ({ url }) => {
+export const load: PageServerLoad = async ({ url, locals }) => {
+	if (!locals.session) {
+		redirect(307, '/login');
+	}
+
 	switch (variables().homepage) {
 		default:
 			redirect(307, browseURL(url.origin));
-			break;
 
 		case 'TAG_LIST':
 			redirect(307, tagURL(url.origin));
-			break;
 
 		case 'BROWSE':
 			redirect(307, browseURL(url.origin));
-			break;
 	}
 };
