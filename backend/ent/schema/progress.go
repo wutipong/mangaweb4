26dc970199ea4cc3d5079920f3f5d0d4dad0e215
Wutipong Wongsakuldej <mr_tawan@hotmail.com>
2026-09-26 14:42:07 +0000

    fix: rename field 'max' to 'count' in Progress schema for clarity

--- backend/ent/schema/progress.go
+++ backend/ent/schema/progress.go
@@ -16,7 +16,7 @@ type Progress struct {
 func (Progress) Fields() []ent.Field {
 	return []ent.Field{
 		field.Int("page").Default(0),
-		field.Int("max").Default(0),
+		field.Int("count").StorageKey("max").Default(0),
 		field.Int("item_id").Optional(),
 		field.Int("user_id").Optional(),
 	}
